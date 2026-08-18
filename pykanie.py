74b3ab3cc9a60455f3145cd66c16f9298208825d
kobiliusz <kobiliusz@gmail.com>
2026-08-18 10:40:29 +0200

    fix xpath

--- pykanie.py
+++ pykanie.py
@@ -80,9 +80,9 @@ def unspoof_referer(driver, addon_id):
 def random_link(driver):
     links = driver.find_elements(
         By.XPATH,
-        '//a[.//p[contains(@data-state,"menu") '
+        '//li[contains(@data-state,"menu") '
         'and contains(@data-state,"selected") '
-        'and contains(@data-state,"link")]]'
+        'and contains(@data-state,"link")]//a[@href]'
     )
 
     if links:
