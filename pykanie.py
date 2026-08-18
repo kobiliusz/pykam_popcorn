e8589bc6d8702d7d1a75f3154848dab5515037ec
kobiliusz <kobiliusz@gmail.com>
2026-08-18 14:28:39 +0200

    fix ref mod

--- pykanie.py
+++ pykanie.py
@@ -79,11 +79,9 @@ def unspoof_referer(driver, addon_id):
 
 def random_link(driver):
     links = driver.find_elements(
-        By.XPATH,
-        '//li[contains(@data-state,"menu") and contains(@data-state,"selected") and contains(@data-state,"link")]//a[@href]'
+        By.CSS_SELECTOR,
+        'wix-dropdown-menu li a[href]'
     )
-
-    ic(links)
 
     return random.choice(links).get_attribute("href")
 
